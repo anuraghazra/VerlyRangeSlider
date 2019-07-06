110e2840df528d85e4101156326ff1b8384d28dc
anuraghazra <hazru.anurag@gmail.com>
2019-07-06 21:02:04 +0530

    added resize handling

--- src/VerlyRange.js
+++ src/VerlyRange.js
@@ -1,3 +1,8 @@
+/**
+ * @author Anurag Hazra
+ * @param {string} id 
+ * @param {string} color 
+ */
 function VerlyRange(id, color) {
   let DOMSlider = document.getElementById(id);
   let canvas = document.createElement('canvas');
@@ -9,32 +14,49 @@ function VerlyRange(id, color) {
   canvas.style.pointerEvents = 'none';
   canvas.style.transform = 'translate(0, -15px)';
   DOMSlider.parentElement.appendChild(canvas);
-
-
+  
+  const gravity = new Vector(0, 0.3);
+  
   // iteration, canvas, ctx
   let verly = new Verly(50, canvas, ctx);
+  let rope = generateRope();
 
-  let rope = verly.createRope(0, 0, width / 20, 17, 0);
-  const lastIndex = rope.points.length - 1;
-  // rope extra tweaks
-  rope.setGravity(new Vector(0, 0.3));
-  rope.pin(lastIndex);
+  // generic function to apply reset of rope wehn resizing
+  function generateRope() {
+    let rope = verly.createRope(0, 0, width / 20, 17, 0);
+    let lastIndex = rope.points.length - 1;
+    // rope extra tweaks
+    rope.setGravity(gravity);
+    rope.pin(lastIndex);
 
-  // rendering
-  rope.renderSticks = () => {
-    for (let i = 0; i < rope.sticks.length; i++) {
-      let stick = rope.sticks[i];
-      ctx.beginPath();
-      ctx.strokeStyle = color;
-      ctx.lineWidth = 10;
-      ctx.lineCap = 'round';
-      ctx.moveTo(stick.startPoint.pos.x, stick.startPoint.pos.y);
-      ctx.lineTo(stick.endPoint.pos.x, stick.endPoint.pos.y);
-      ctx.stroke();
-      ctx.closePath();
+    // overwrite render function
+    rope.renderSticks = () => {
+      for (let i = 0; i < rope.sticks.length; i++) {
+        let stick = rope.sticks[i];
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.moveTo(stick.startPoint.pos.x, stick.startPoint.pos.y);
+        ctx.lineTo(stick.endPoint.pos.x, stick.endPoint.pos.y);
+        ctx.stroke();
+        ctx.closePath();
+      }
     }
+    return rope;
   }
 
+  // handle resize 
+  window.addEventListener('resize', function () {
+    width = DOMSlider.scrollWidth
+    height = width / 2;
+    canvas.width = verly.WIDTH = width;
+    canvas.height = verly.HEIGHT = height;
+
+    rope = generateRope();
+
+    setRopePosition();
+  });
 
   // rope end point position
   function setRopePosition() {
@@ -44,11 +66,10 @@ function VerlyRange(id, color) {
     if (ratio < 0.3) ratio += 0.01;
     if (ratio > 0.6) ratio -= 0.01;
     if (ratio > 0.8) ratio -= 0.02;
-    rope.points[lastIndex].pos.x = (ratio * width);
+    rope.points[rope.points.length - 1].pos.x = (ratio * width);
   }
   setRopePosition();
   DOMSlider.addEventListener('input', setRopePosition);
-
 
   function animate() {
     ctx.clearRect(0, 0, width, height);
